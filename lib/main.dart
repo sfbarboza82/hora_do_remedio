fcd690134763205be7e735d46193caf8ef94d872
Sérgio Fabrício Barboza <sfbarboza82@gmail.com>
2021-09-16 17:27:33 -0300

    criacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hora_do_remedio/view_widget/HomeView.dart';
+import 'package:hora_do_remedio/view_widget/LoginView.dart';
 
 
 
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: LoginView(),
+    theme: ThemeData(
+      primaryColor: Color(0xff075E54),
+          accentColor: Color(0xff25D366)
+    ),
+    debugShowCheckedModeBanner: false,
+
+  ));
 }
---
+++ lib/view_widget/LoginView.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(color: Color(0xff075E54)),
+          padding: EdgeInsets.all(16),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch ,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 32),
+                    child: Image.asset(
+                      "images/logo.png",
+                      width: 200,
+                      height: 150,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: TextField(
+                      autofocus: true,
+                      keyboardType: TextInputType.emailAddress,
+                      style: TextStyle(fontSize: 20),
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                          hintText: "E-mail",
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(32))),
+                    ),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Senha",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16, bottom: 10),
+                    child: RaisedButton(
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      color: Colors.green,
+                      padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32)
+                      ),
+                      onPressed: () {}),
+                  ),
+                  Center(
+                    child: GestureDetector(
+                      child: Text(
+                          "Não tem conta? Cadastre-se!",
+                          style: TextStyle(
+                            color: Colors.white
+                          )
+                      ),
+                      onTap: (){
+
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
