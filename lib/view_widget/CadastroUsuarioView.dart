75361150002b7acd7a1c6b264be919a67e64a7f7
Sérgio Fabrício Barboza <sfbarboza82@gmail.com>
2021-09-22 18:10:11 -0300

    tela de cadastro

--- lib/view_widget/CadastroUsuarioView.dart
+++ lib/view_widget/CadastroUsuarioView.dart
@@ -1,119 +1,204 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hora_do_remedio/model/UsuarioModel.dart';
 
 class CadastroUsuarioView extends StatefulWidget {
-  const CadastroUsuarioView({Key? key}) : super(key: key);
-
   @override
   _CadastroUsuarioViewState createState() => _CadastroUsuarioViewState();
 }
 
 class _CadastroUsuarioViewState extends State<CadastroUsuarioView> {
+
+  TextEditingController _controllerNome = TextEditingController(text: "Sérgio Barboza");
+  TextEditingController _controllerEmail = TextEditingController(text: "sfbarboza82@gmail.com");
+  TextEditingController _controllerSenha = TextEditingController(text: "1234567");
+  bool _tipoUsuario = false;
+  String _mensagemErro = "";
+
+  _validarCampos(){
+
+    //Recuperar dados dos campos
+    String nome = _controllerNome.text;
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    //validar campos
+    if( nome.isNotEmpty ){
+
+      if( email.isNotEmpty && email.contains("@") ){
+
+        if( senha.isNotEmpty && senha.length > 6 ){
+
+          UsuarioModel usuario = UsuarioModel();
+          usuario.nome = nome;
+          usuario.email = email;
+          usuario.senha = senha;
+          usuario.tipoUsuario = usuario.verificaTipoUsuario(_tipoUsuario);
+
+          _cadastrarUsuario( usuario );
+
+        }else{
+          setState(() {
+            _mensagemErro = "Preencha a senha! digite mais de 6 caracteres";
+          });
+        }
+
+      }else{
+        setState(() {
+          _mensagemErro = "Preencha o E-mail válido";
+        });
+      }
+
+    }else{
+      setState(() {
+        _mensagemErro = "Preencha o Nome";
+      });
+    }
+
+  }
+
+  _cadastrarUsuario( UsuarioModel usuario ){
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+    Firestore db = Firestore.instance;
+
+    auth.createUserWithEmailAndPassword(
+        email: usuario.email,
+        password: usuario.senha
+    ).then((firebaseUser){
+
+      db.collection("usuarios")
+          .document( firebaseUser.user.uid )
+          .setData( usuario.toMap() );
+
+      //redireciona para o painel, de acordo com o tipoUsuario
+      switch( usuario.tipoUsuario ){
+        case "medico" :
+          Navigator.pushNamedAndRemoveUntil(
+              context,
+              "/painel-medico",
+                  (_) => false
+          );
+          break;
+        case "paciente" :
+          Navigator.pushNamedAndRemoveUntil(
+              context,
+              "/painel-paciente",
+                  (_) => false
+          );
+          break;
+      }
+
+    }).catchError((error){
+      _mensagemErro = "Erro ao cadastrar usuário, verifique os campos e tente novamente!";
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cadastro de Usúario"),
+        title: Text("Cadastro de Usuario"),
       ),
       body: Container(
-          decoration: BoxDecoration(color: Color(0xff075E54)),
-          padding: EdgeInsets.all(16),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 32),
-                    child: Image.asset(
-                      "images/logo.png",
-                      width: 200,
-                      height: 150,
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextField(
+                  controller: _controllerNome,
+                  autofocus: true,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Nome completo",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+                TextField(
+                  controller: _controllerEmail,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "e-mail",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+                TextField(
+                  controller: _controllerSenha,
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "senha",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Text("Paciente"),
+                      Switch(
+                          value: _tipoUsuario,
+                          onChanged: (bool valor){
+                            setState(() {
+                              _tipoUsuario = valor;
+                            });
+                          }
+                      ),
+                      Text("Medico"),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 10),
+                  child: RaisedButton(
+                      child: Text(
+                        "Cadastrar",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      color: Color(0xff1ebbd8),
+                      padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      onPressed: (){
+                        _validarCampos();
+                      }
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Center(
+                    child: Text(
+                      _mensagemErro,
+                      style: TextStyle(color: Colors.red, fontSize: 20),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8),
-                    child: TextField(
-                      autofocus: true,
-                      keyboardType: TextInputType.text,
-                      style: TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
-                          contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                          hintText: "Nome",
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(32))),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8),
-                    child: TextField(
-                      keyboardType: TextInputType.text,
-                      style: TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
-                          contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                          hintText: "Endereço",
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(32))),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 8),
-                    child: TextField(
-                      autofocus: true,
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
-                          contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                          hintText: "E-mail",
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(32))),
-                    ),
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(fontSize: 20),
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        hintText: "Senha",
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(32))),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 16, bottom: 10),
-                    child: RaisedButton(
-                        child: Text(
-                          "Entrar",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                        color: Colors.green,
-                        padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(32)),
-                        onPressed: () {}),
-                  ),
-                  Center(
-                    child: GestureDetector(
-                      child: Text("Não tem conta? Cadastre-se!",
-                          style: TextStyle(color: Colors.white)),
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => CadastroUsuarioView()));
-                      },
-                    ),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
